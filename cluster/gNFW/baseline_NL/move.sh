2de2f88ff471141619e0677d79919c54439db524
mariabenitocst <mariabenitocst@gmail.com>
2023-12-18 12:52:54 +0200

    rm

--- cluster/gNFW/baseline_NL/move.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for rank in {1..100..1}
-do
-    echo ${rank}
-    #mkdir /local/mariacst/2022_exoplanets/results/gNFW/baseline_NL_longer/${rank}/
-    mv out/${rank}/baseline_NL_gNFW_longerPriorG_N1000_*post_equal_weights.dat /local/mariacst/2022_exoplanets/results/gNFW/baseline_NL_longer/${rank}/
-done
